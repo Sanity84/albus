3d13e377d9da44318b5de0b5566601231e3e6f1e
Andrew Torrez <atorrez@outlook.com>
2015-05-19 17:45:04 -0400

    comments added

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ define('ROUTE_DIR', ROOT.DS.'albus'.DS.'Routes'.DS.'*.php'); // may need to be t
 
 require ROOT.DS.'albus'.DS.'Core'.DS.'autoloader.php';
 
+// All user defined routes should be defined in albus/Routes/<filename>.php
 foreach (glob(ROUTE_DIR) as $userRoute) {
     require $userRoute;
 }
